106ca0e764e3b9a0f8ed2b6d6611e805e08f35a8
Juliano <julianosilva82010@gmail.com>
2019-06-17 16:22:35 -0400

    start text robot

---
+++ index.js
@@ -0,0 +1,29 @@
+const readline = require('readline-sync');
+const robots = {
+	text: require('./robots/text'),
+}
+
+async function start(){
+	function askAndReturnSearchTerm(){
+		return readline.question('Type a Wikipedia search term: ');
+	}
+
+	function askAndReturnPrefix(){
+		const prefixes = ['Who is', 'What is', 'The history off'];
+		const selectedPrefixIndex = readline.keyInSelect(prefixes,'Choose an option');
+		const selectedPrefixText = prefixes[selectedPrefixIndex];
+
+		return selectedPrefixText; 
+	}
+
+	const content = {};
+
+	content.searchItem = askAndReturnSearchTerm();
+	content.prefix = askAndReturnPrefix();
+
+	await robots.text(content);
+
+	// console.log(content);
+}
+
+start();

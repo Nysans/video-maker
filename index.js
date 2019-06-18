0324d36dfa5da99457e9daf9d34eea2f1f5a1f6a
Juliano <julianosilva82010@gmail.com>
2019-06-18 14:15:33 -0400

    finish Watson integration

--- index.js
+++ index.js
@@ -3,27 +3,29 @@ const robots = {
 	text: require('./robots/text'),
 }
 
-async function start(){
-	function askAndReturnSearchTerm(){
-		return readline.question('Type a Wikipedia search term: ');
-	}
-
-	function askAndReturnPrefix(){
-		const prefixes = ['Who is', 'What is', 'The history off'];
-		const selectedPrefixIndex = readline.keyInSelect(prefixes,'Choose an option');
-		const selectedPrefixText = prefixes[selectedPrefixIndex];
-
-		return selectedPrefixText; 
-	}
-
-	const content = {};
+async function start() {
+	const content = {
+		maximumSentences: 7
+	};
 
 	content.searchItem = askAndReturnSearchTerm();
 	content.prefix = askAndReturnPrefix();
 
 	await robots.text(content);
 
-	// console.log(content);
+	// console.log(JSON.stringify(content, null, 4));
+}
+
+function askAndReturnSearchTerm() {
+	return readline.question('Type a Wikipedia search term: ');
+}
+
+function askAndReturnPrefix() {
+	const prefixes = ['Who is', 'What is', 'The history off'];
+	const selectedPrefixIndex = readline.keyInSelect(prefixes, 'Choose an option');
+	const selectedPrefixText = prefixes[selectedPrefixIndex];
+
+	return selectedPrefixText;
 }
 
 start();
--- robots/text.js
+++ robots/text.js
@@ -1,59 +1,103 @@
 const algorithmia = require('algorithmia');
 const algorithmiaApiKey = require('../credentials/algorithmia.json').apiKey;
+
 const sentenceBoundaryDetection = require('sbd');
 
-async function robot(content) {
+const watsonApiKey = require('../credentials/watson-nlu.json').apikey
+const NaturalLanguageUnderstandingV1 = require('watson-developer-cloud/natural-language-understanding/v1.js');
 
+var nlu = new NaturalLanguageUnderstandingV1({
+    iam_apikey: watsonApiKey,
+    version: '2018-04-05',
+    url: 'https://gateway.watsonplatform.net/natural-language-understanding/api/'
+});
+
+async function robot(content) {
     await fetchContentFromWikipedia(content);
     sanitizeContent(content);
     breakContentIntoSentence(content);
+    limiteMaximumSentences(content);
+    await fetchKeywordOfAllSentences(content);
+}
 
-    async function fetchContentFromWikipedia(content) {
-        const algorithmiaAuthenticated = algorithmia(algorithmiaApiKey);
-        const wikipediaAlgorithn = algorithmiaAuthenticated.algo('web/WikipediaParser/0.1.2');
-        const wikipediaResponse = await wikipediaAlgorithn.pipe(content.searchItem);
-        const wikipediaContent = wikipediaResponse.get();
-
-        content.sourceContentOriginal = wikipediaContent.content;
-
-    }
-
-    function sanitizeContent(content) {
-        const withoutBlankLinesAndMarkdown = removeBlanklinesAndMarkdown(content.sourceContentOriginal);
-        const withoutDatesInParenthesis = removeDatesInParenthesis(withoutBlankLinesAndMarkdown);
-        content.sourceContentSanitized = withoutDatesInParenthesis;
-
-        function removeBlanklinesAndMarkdown(text) {
-            const allLines = text.split('\n');
-
-            const withoutBlankLinesAndMarkdown = allLines.filter((line) => {
-                const trimmedLine = line.trim();
-                if (trimmedLine.length === 0 || trimmedLine.startsWith('=')) {
-                    return false;
+async function fetchWatsonAndReturnKeywords(sentence) {
+    return new Promise((resolve, reject) => {
+        nlu.analyze(
+            {
+                text: sentence,
+                features: {
+                    keywords: {}
                 }
-                return true;
-            });
+            },
+            function (err, response) {
+                if (err) {
+                    throw err;
+                } else {
+                    const keywords = response.keywords.map((keyword) => {
+                        return keyword.text;
+                    });
+                    resolve(keywords);
+                }
+            }
+        );
+    });
+}
 
-            return withoutBlankLinesAndMarkdown.join(' ');
-        }
+async function fetchContentFromWikipedia(content) {
+    const algorithmiaAuthenticated = algorithmia(algorithmiaApiKey);
+    const wikipediaAlgorithn = algorithmiaAuthenticated.algo('web/WikipediaParser/0.1.2');
+    const wikipediaResponse = await wikipediaAlgorithn.pipe(content.searchItem);
+    const wikipediaContent = wikipediaResponse.get();
 
-        function removeDatesInParenthesis(text){
-            return text.replace(/\((?:\([^()]*\)|[^()])*\)/gm, '').replace(/  /g,' ');
-        }
+    content.sourceContentOriginal = wikipediaContent.content;
+
+}
+
+function sanitizeContent(content) {
+    const withoutBlankLinesAndMarkdown = removeBlanklinesAndMarkdown(content.sourceContentOriginal);
+    const withoutDatesInParenthesis = removeDatesInParenthesis(withoutBlankLinesAndMarkdown);
+    content.sourceContentSanitized = withoutDatesInParenthesis;
+
+    function removeBlanklinesAndMarkdown(text) {
+        const allLines = text.split('\n');
+
+        const withoutBlankLinesAndMarkdown = allLines.filter((line) => {
+            const trimmedLine = line.trim();
+            if (trimmedLine.length === 0 || trimmedLine.startsWith('=')) {
+                return false;
+            }
+            return true;
+        });
+
+        return withoutBlankLinesAndMarkdown.join(' ');
     }
 
-    function breakContentIntoSentence(content){
-        content.sentences = [];
+    function removeDatesInParenthesis(text) {
+        return text.replace(/\((?:\([^()]*\)|[^()])*\)/gm, '').replace(/  /g, ' ');
+    }
+}
 
-        const sentences =  sentenceBoundaryDetection.sentences(content.sourceContentSanitized);
+function breakContentIntoSentence(content) {
+    content.sentences = [];
 
-        sentences.forEach((sentence)=> {
-            content.sentences.push({
-                text: sentence, 
-                keywords: [], 
-                images: []
-            });
+    const sentences = sentenceBoundaryDetection.sentences(content.sourceContentSanitized);
+
+    sentences.forEach((sentence) => {
+        content.sentences.push({
+            text: sentence,
+            keywords: [],
+            images: []
         });
+    });
+}
+
+function limiteMaximumSentences(content) {
+    content.sentences = content.sentences.slice(0, content.maximumSentences);
+}
+
+async function fetchKeywordOfAllSentences(content){
+    for(const sentence of content.sentences){
+        sentence.keywords = await fetchWatsonAndReturnKeywords(sentence.text);
     }
 }
 
